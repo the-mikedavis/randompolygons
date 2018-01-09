18ac01a0c8e886b442e0ab92e83ca1d008f30993
Michael Davis <rubiksmaster65@yahoo.com>
2018-01-09 08:17:25 -0500

    Wrap the Line2D intersect method

--- poly-gen/src/poly_gen/line.clj
+++ poly-gen/src/poly_gen/line.clj
@@ -1,4 +1,5 @@
-(ns poly-gen.line)
+(ns poly-gen.line
+  (:import [java.awt.geom Line2D]))
 
 (defn make
   "Create a line from vertices"
@@ -6,7 +7,7 @@
   (list (list xa ya) (list xb yb))) ; explicitly defined for clarity
 
 (defn vertices->lines
-  "Morph a polygons points into a list of lines"
+  "Morph a polygon's list of points into a list of lines"
   [points]
   (loop [lines []
          recent (first points)
@@ -16,3 +17,7 @@
       (recur (conj lines (make recent (first pts)))
              (first pts) (rest pts)))))
 
+(defn intersect?
+  "Tests if two lines intersect using vector cross product"
+  [[[x1 y1] [x2 y2]]  [[x3 y3] [x4 y4]]]
+  (Line2D/linesIntersect x1 y1 x2 y2 x3 y3 x4 y4))
