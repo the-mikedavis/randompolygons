8702caa203d637c6a7803f43c6651c2f161e7f17
Michael Davis <rubiksmaster65@yahoo.com>
2018-01-08 22:04:57 -0500

    Add base files for a clojure implementation

---
+++ poly-gen/src/poly_gen/core.clj
@@ -0,0 +1,7 @@
+(ns poly-gen.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ poly-gen/test/poly_gen/core_test.clj
@@ -0,0 +1,7 @@
+(ns poly-gen.core-test
+  (:require [clojure.test :refer :all]
+            [poly-gen.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
